9989ae577a14f8e9763e91a0771dfd9a65a20fc7
Syuichi Kumagai <kumagaisyuichi@gmail.com>
2022-11-18 13:31:50 +0800

    Build the first three blocks of the snake body and control its movements

---
+++ snake.py
@@ -0,0 +1,44 @@
+from turtle import Turtle
+MOVE_DISTANCE = 20
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = (self.segments[0])
+
+    def create_snake(self):
+        for square in STARTING_POSITIONS:
+            new_square = Turtle(shape="square")
+            new_square.color("white")
+            new_square.penup()
+            new_square.goto(square)
+            self.segments.append(new_square)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
